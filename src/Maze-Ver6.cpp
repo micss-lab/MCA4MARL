30c5c9f81d40cb48597dd0ad5ac3c4a757dd7790
jonasdemaeyer1 <Jonas.DeMaeyer@student.uantwerpen.be>
2025-06-10 22:28:32 +0200

    Ready for experiments

--- src/Maze-Ver6.cpp
+++ src/Maze-Ver6.cpp
@@ -2393,7 +2393,7 @@ public:
 
 /*************************************************************************/
 void runFullExperiment() {
-    vector<int> sizes = {50, 100, 200, 300};
+    vector<int> sizes = {20, 50, 100, 200, 300};
     vector<tuple<double, double, double> > difficulties = {
         {0.8, 0.18, 0.02}, // Easy
         {0.7, 0.29, 0.01}, // Medium
